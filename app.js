ecd9f26c8b87a6d36cf3a45c5a38a0f9afe21d90
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-05-12 02:25:28 +1000

    Set column width to 80 chars

--- app.js
+++ app.js
@@ -25,12 +25,14 @@ bot.on('message', (message) => {
         }
         sendMessage () {
             if (this.amount <= 1) {
-                message.channel.send(getTargetString(targets) + " fortnite?").then((message) => {
+                message.channel.send(getTargetString(targets) + " fortnite?")
+                    .then((message) => {
                     message.delete(3600000);
                 });
                 return;
             }
-            message.channel.send(getTargetString(targets) + " fortnite? " + (this.amount - 1) + " left").then((message) => {
+            message.channel.send(getTargetString(targets) + " fortnite? " + 
+                (this.amount - 1) + " left").then((message) => {
                 message.delete(3600000);
             });
         }
@@ -53,7 +55,8 @@ bot.on('message', (message) => {
     let getTargetString = (targets) => {
         let targetString = "";
         for (let i = 0; i < targets.length; i++) {
-            targetString += i < targets.length - 1 ? targets[i] + " " : targets[i];
+            targetString += i < targets.length - 1 ? 
+                targets[i] + " " : targets[i];
         }
         return targetString;
     };
@@ -69,33 +72,42 @@ bot.on('message', (message) => {
 
     let messageStyles = [
         "OwO someone said fortnite? " + getTargetString(targets) + " fortnite?",
-        "AwoooooOOoo someone said fortnite? " + getTargetString(targets) + " fortnite?",
+        "AwoooooOOoo someone said fortnite? " + getTargetString(targets) + 
+            " fortnite?",
         "Someone said fortnite :3 " + getTargetString(targets) + " fortnite?",
         "Fortnite? " + getTargetString(targets) + " fortnite?",
-        "How do I draw triangles with vector transforms? " + getTargetString(targets) + " fortnite?",
-        "Its..its not like I wan...want to play fortnite with " + getTargetString(targets) + " or anything >///<",
-        "Notices fortnite, Owo whats this? " + getTargetString(targets) + " fortnite?"
+        "How do I draw triangles with vector transforms? " + 
+            getTargetString(targets) + " fortnite?",
+        "Its..its not like I wan...want to play fortnite with " + 
+            getTargetString(targets) + " or anything >///<",
+        "Notices fortnite, Owo whats this? " + getTargetString(targets) + 
+            " fortnite?"
     ];
 
     if (Math.random() < 0.025) {
         if (Math.random < 0.5) {
             chatBot.create((err, session) => {
                 chatBot.ask(message.content, (err, res) => {
-                    let response = res + " " + messageStyles[randInt(0, messageStyles.length - 1)];
+                    let response = res + " " + messageStyles[randInt(0, 
+                        messageStyles.length - 1)];
                     message.channel.send(response).then((message) => {
                         message.delete(3600000);
                     });
                 });
             });
         } else {
-            request("https://talaikis.com/api/quotes/random/", (err, res, body) => {
+            request("https://talaikis.com/api/quotes/random/", 
+                (err, res, body) => {
                 let quoteRes = JSON.parse(body);
                 let style = messageStyles[randInt(0, messageStyles.length - 1)];
                 let response;
                 if (quoteRes.quote.substr(quoteRes.quote.length - 1) == ".")
-                    response = quoteRes.author + " said - " + quoteRes.quote.slice(0, quoteRes.quote.length - 1) 
-                        + " and " + style.substr(0, 1).toLowerCase() + style.substr(1);
-                response = quoteRes.author + " said - " + quoteRes.quote + style;
+                    response = quoteRes.author + " said - " + quoteRes.quote
+                        .slice(0, quoteRes.quote.length - 1) +
+                        " and " + style.substr(0, 1).toLowerCase() +
+                        style.substr(1);
+                response = quoteRes.author + " said - " + quoteRes.quote +
+                    style;
                 message.channel.send(response).then((message) => {
                     message.delete(3600000);
                 });
@@ -103,13 +115,17 @@ bot.on('message', (message) => {
         }
     }
 
-    if (message.content.replace(/\s/g, '').toLowerCase().search("pubg") != -1 && !message.author.bot && message.content[0] != "!") {
-        message.channel.send("PUBG aka PlayerUnknown's Battle Grounds is trash, Fortnite is better ^__^ " + getTargetString(targets) + " fortnite?").then((message) => {
+    if (message.content.replace(/\s/g, '').toLowerCase().search("pubg") != -1 &&
+         !message.author.bot && message.content[0] != "!") {
+        message.channel.send("PUBG aka PlayerUnknown's Battle Grounds is " +
+            "trash, Fortnite is better ^__^ " + getTargetString(targets) + 
+            " fortnite?").then((message) => {
                 message.delete(3600000);
         });
     }
 
-    if ((message.content.replace(/\s/g, '').toLowerCase().search("fortnite") != -1) && (!message.author.bot && message.content[0] != "!")) {
+    if ((message.content.replace(/\s/g, '').toLowerCase().search("fortnite") != 
+        -1) && (!message.author.bot && message.content[0] != "!")) {
         let response = messageStyles[randInt(0, messageStyles.length - 1)];
         message.channel.send(response).then((message) => {
             message.delete(3600000);
@@ -136,8 +152,11 @@ bot.on('message', (message) => {
             let amount = args[2];
             loop = new Loop(parseInt(amount), parseInt(time));
             loop.startLoop();
-            let formattedTime = moment.duration(parseInt(time), "milliseconds").format();
-            message.channel.send("Auto fortnite set. I will ping " + getTargetString(targets) + " once every " + formattedTime + " for " + amount + " times").then((message) => {
+            let formattedTime = moment.duration(parseInt(time), "milliseconds")
+                .format();
+            message.channel.send("Auto fortnite set. I will ping " +
+                getTargetString(targets) + " once every " + formattedTime +
+                " for " + amount + " times").then((message) => {
                 message.delete(3600000);
             });
 
@@ -148,25 +167,33 @@ bot.on('message', (message) => {
         else if (command == "target") {
             if (targets)
                 targets = args.splice(2);
-                message.channel.send("New targets set, don't be mad " + getTargetString(targets));
+                message.channel.send("New targets set, don't be mad " +
+                    getTargetString(targets));
         }
         else if (command == "delete") {
-            message.channel.fetchMessages({limit: parseInt(args[2])}).then((messages) => {
-                let selfMessages = messages.filter((messages) => (messages.author.bot));
+            message.channel.fetchMessages({limit: parseInt(args[2])})
+                .then((messages) => {
+                let selfMessages = messages.filter((messages) =>
+                    (messages.author.bot));
                 let amount = selfMessages.array().length;
                 message.channel.bulkDelete(selfMessages);
-                message.channel.send("Deleted " + amount + " messages ^__^ " + getTargetString(targets) + " wanna play fortnite?");
+                message.channel.send("Deleted " + amount + " messages ^__^ " +
+                    getTargetString(targets) + " wanna play fortnite?");
             });
         }
         else if (command == "help") {
-            message.channel.send("```OwO u wan halp?\n"
-            + "!fortnite - Asks your targets to play fortnite. >///<\n"
-            + "!fortnite tts - Asks your targets to play fortnite but more nicely. >/////<\n"
-            + "!fortnite target {@target1} {@target2} ... {@targetn} - Set new targets/friends :3\n"
-            + "!fortnite auto {amount} {delay(ms)} - Ask your friends many many manie times\n"
-            + "!fortnite delete - Removes my nice messages ლ(´ڡ`ლ)\n"
-            + "!fortnite stop - stops any pings caused by !fortnite auto (may be delayed) ᕙ(⇀‸↼‶)ᕗ"
-            + "```"
+            message.channel.send("```OwO u wan halp?\n" +
+            "!fortnite - Asks your targets to play fortnite. >///<\n" +
+            "!fortnite tts - Asks your targets to play fortnite but more " +
+                " nicely. >/////<\n" +
+            "!fortnite target {@target1} {@target2} ... {@targetn} - Set new " +
+                " targets/friends :3\n" +
+            "!fortnite auto {amount} {delay(ms)} - Ask your friends many " +
+                " many manie times\n" +
+            "!fortnite delete - Removes my nice messages ლ(´ڡ`ლ)\n" +
+            "!fortnite stop - stops any pings caused by !fortnite auto " +
+                " (may be delayed) ᕙ(⇀‸↼‶)ᕗ" +
+            "```"
             );
         }
         else {
@@ -179,5 +206,6 @@ bot.on('message', (message) => {
 
 bot.login(authToken);
 bot.on("ready", () => {
-    bot.user.setGame("PLAYERUNKNOWN'S BATTLEGROUNDS", "https://www.twitch.tv/yaxises");
+    bot.user.setGame("PLAYERUNKNOWN'S BATTLEGROUNDS",
+        "https://www.twitch.tv/yaxises");
 });
