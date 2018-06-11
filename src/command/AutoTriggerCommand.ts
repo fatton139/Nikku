844cd2cf613fbf17a64647a8b8abed9f260aa9e6
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-06-12 01:43:08 +1000

    Implement auto trigger commands

--- src/command/AutoTriggerCommand.ts
+++ src/command/AutoTriggerCommand.ts
@@ -1,4 +1,23 @@
 import { ICommand } from "../command/ICommand";
 import { Command } from "../command/Command";
+
+/**
+ * Commands which are auto triggered.
+ */
+
 export class AutoTriggerCommand extends Command implements ICommand {
+    public trigger: () => boolean;
+    public constructor(action: () => boolean, trigger: () => boolean) {
+        super(action, null);
+        this.trigger = trigger;
+    }
+    public tryTrigger(): boolean {
+        if (this.trigger) {
+            if (!this.executeAction()) {
+                throw new Error("Failed Command");
+            }
+            return true;
+        }
+        return false;
+    }
 }
