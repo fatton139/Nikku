5165902984a5a860822ea064f87b3cac7c15fcf2
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-06-12 01:42:48 +1000

    Add additional command types

---
+++ src/command/AutoTriggerCommand.ts
@@ -0,0 +1,4 @@
+import { ICommand } from "../command/ICommand";
+import { Command } from "../command/Command";
+export class AutoTriggerCommand extends Command implements ICommand {
+}
---
+++ src/command/ExecutableCommand.ts
@@ -0,0 +1,10 @@
+import { ICommand } from "../command/ICommand";
+import { Command } from "../command/Command";
+
+/**
+ * Commands which must be executed by a user to run.
+ */
+
+export class ExecutableCommand extends Command implements ICommand {
+
+}
