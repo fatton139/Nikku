52288b2deb0f10fc446f70b66481d232befc58bb
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-06-14 00:52:44 +1000

    Pass state as paramater to trigger callbacks

--- src/action/FortniteBotTrigger.ts
+++ src/action/FortniteBotTrigger.ts
@@ -1,10 +1,11 @@
 import { FortniteBotAction } from "./FortniteBotAction";
+import { FortniteBotState } from "../state/FortniteBotState";
 
 export class FortniteBotTrigger extends FortniteBotAction {
-    public constructor(trigger: () => boolean) {
+    public constructor(trigger: (state: FortniteBotState) => boolean) {
         super(0, trigger);
     }
-    public execute(): boolean {
-        return super.execute(null, null);
+    public execute(state: FortniteBotState): boolean {
+        return super.execute(state, null);
     }
 }
