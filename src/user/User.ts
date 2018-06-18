3be6daf5801abfaccee5d10a05715f7636b19f4b
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-06-18 20:03:18 +1000

    Added more members to user class

--- src/user/User.ts
+++ src/user/User.ts
@@ -3,10 +3,17 @@ import { IUser } from "../user/IUser";
 export class User implements IUser {
     public readonly id: string;
     public accessLevel: number;
-    public name: string;
-    constructor(id: string, accessLevel: number, name: string) {
+    public currency: {
+        DotmaCoin: number;
+        BradCoin: number;
+    };
+    constructor(id: string, accessLevel: number) {
         this.id = id;
         this.accessLevel = accessLevel;
-        this.name = name;
+        this.currency = {
+            DotmaCoin: 100,
+            BradCoin: 0
+        };
+        this.title = "The Undefined";
     }
 }
