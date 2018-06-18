d349e14e3b94d9b166732e9c6e0d1743ea37732c
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-06-18 23:25:42 +1000

    Added increment method

--- src/database/UserCollection.ts
+++ src/database/UserCollection.ts
@@ -37,6 +37,19 @@ export class UserCollection extends FrotniteBotCollection implements ICollection
             callback(true);
         });
     }
+    public incrementCoin(userId: string, field: string, value: number,
+                         callback: (res: boolean) => void): void {
+        this.get((res: User[]) => {
+            const index = res.findIndex((user: User) => user.id === userId);
+            if (index === -1) {
+                return;
+            }
+            const newVal = res[index].currency[field] + value;
+            this.update(userId, "currency." + field, newVal, (c: boolean) => {
+                callback(c);
+            });
+        });
+    }
     public removeUser(userId: string, callback: (res: boolean) => void): void {
         this.db.collection("user").deleteOne({id: userId}, (err) => {
             if (err) {
