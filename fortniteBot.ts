14e87b4e337e496ae11ee006b8f69425bac178d1
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-06-19 17:49:13 +1000

    Added fields for deployment

--- fortniteBot.ts
+++ fortniteBot.ts
@@ -3,14 +3,19 @@ import "discord.js";
 import { FortniteBotCore } from "./src/core/FortniteBotCore";
 import { FortniteBotCommandConfig } from "./src/config/FortniteBotCommandConfig";
 import { FortniteBotInitConfig } from "./src/config/FortniteBotInitConfig";
-import { FortniteBotDbConfig } from "./src/config/FortniteBotDbConfig"; 
+import { FortniteBotDbConfig } from "./src/config/FortniteBotDbConfig";
 
-import "./temp";
+const keys = {
+    discordToken: process.env.discordToken,
+    chatBotUserId: process.env.chatBotUserId,
+    chatBotAPIKey: process.env.chatBotAPIKey
+};
+
 
 const initConfig = new FortniteBotInitConfig(keys.discordToken,
     keys.chatBotUserId, keys.chatBotAPIKey);
 
-export const fortniteBotDbConfig = new FortniteBotDbConfig("--");
+export const fortniteBotDbConfig = new FortniteBotDbConfig(process.env.dbIp);
 export const fortniteBotCore = new FortniteBotCore(initConfig);
 
 fortniteBotCore.start();
