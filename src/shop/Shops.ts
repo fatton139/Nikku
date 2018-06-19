2e0a9703ebc12ea0759b735d26ca8c46f5c1883f
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-06-20 03:31:12 +1000

    Added some basic inventory

---
+++ src/shop/Shops.ts
@@ -0,0 +1,24 @@
+import { Shop } from "./Shop";
+import { Item } from "./Item";
+
+export const Shops = [
+    new Shop("Title Shop", "DotmaCoin", "Buy titles for your profile."),
+    new Shop("Brad Shop", "BradCoin", "Some fat kid sells stuff here.")
+];
+
+Shops[0].addItems([
+    new Item("The Knight", 150),
+    new Item("The Baron", 150),
+    new Item("The Egg", 300),
+    new Item("The Dum kid", 300),
+    new Item("The Apple Employee", 500),
+    new Item("The Maccas Employee", 500),
+    new Item("The QUT kid", 700),
+    new Item("The UQ kid", 1000),
+    new Item("The King", 1500),
+    new Item("The Boss", 1500),
+    new Item("The Fat kid", 5, "BradCoin"),
+    new Item("The Brad", 5, "BradCoin"),
+    new Item("YOUR CUSTOM TITLE", 50, "BradCoin")
+]);
+
