2d1fe292e8990d3223e6fd95f748514e9a49a553
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-06-21 20:22:26 +1000

    Remove object type and added handle updates

--- src/state/FortniteBotState.ts
+++ src/state/FortniteBotState.ts
@@ -1,14 +1,18 @@
 import * as Discord from "discord.js";
+import { fortniteBotCore as activeCore } from "../../fortniteBot";
 
 export class FortniteBotState {
-    private handle: object;
+    public handle: any;
     constructor(handle: object) {
         this.handle = handle;
     }
     public setHandle(handle: object): void {
         this.handle = handle;
     }
-    public getHandle(): object {
+    public getHandle(): any {
         return this.handle;
+    }
+    public updateHandle(): any {
+        return activeCore.getCoreState().getHandle();
     }
 }
