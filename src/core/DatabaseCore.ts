0d96e4711b6532b9d6eb58c485abf857e1d3a8c4
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-11-21 01:15:00 +1000

    Setup db core using new packages

--- src/core/DatabaseCore.ts
+++ src/core/DatabaseCore.ts
@@ -1,28 +1,22 @@
 import * as MongoDb from "mongodb";
 import * as Mongoose from "mongoose";
-import { MongoClient } from "mongodb";
-import { DatabaseException } from "../exceptions/DatabaseException";
-import { GlobalCollection } from "../database/GlobalCollection";
-import { UserCollection } from "../database/UserCollection";
-import { Config } from "config/Config";
-import { DatabaseSchema } from "database/schemas/UserSchema";
+import { DBUserSchema } from "database/schemas/DBUserSchema";
+import { DBDateTracker } from "database/schemas/DBDateTracker";
+import { Logger } from "logger/Logger";
+import winston = require("winston");
+import { UserMigrator } from "database/mirgration/UserMigrator";
 
 export class DatabaseCore {
-    /**
-     * Collections which store other forms of data.
-     */
-    public collections: {
-        global: GlobalCollection;
-        user: UserCollection;
-    };
-
+    private readonly logger: winston.Logger = new Logger(this.constructor.name).getLogger();
     private readonly URL: string;
     /**
-     * Database object to interact with.
+     * Database connection.
      */
     private connection: Mongoose.Connection;
 
     private UserModel: Mongoose.Model<Mongoose.Document, {}>;
+    private DateTrackerModel: Mongoose.Model<Mongoose.Document, {}>;
+
     /**
      * @classdesc Class for handling important database methods.
      */
@@ -40,15 +34,19 @@ export class DatabaseCore {
             throw new Error("Connection Error");
         });
         this.connection.once("open", () => {
-            this.UserModel = new DatabaseSchema.User().getModelForClass(DatabaseSchema.User);
+            const userSchema: DBUserSchema = new DBUserSchema();
+            this.UserModel = userSchema.getModelForClass(DBUserSchema);
             this.UserModel.find({}).then((doc) => {
-                console.log(doc);
                 if (doc.length === 0) {
-                    return;
+                    this.logger.warn("User model has not been setup. Creating default profiles...");
                 }
             });
-        }).catch((err) => {
-            throw new DatabaseException(err);
+            this.DateTrackerModel = new DBDateTracker().getModelForClass(DBDateTracker);
+            this.DateTrackerModel.find({}).then((doc) => {
+                if (doc.length === 0) {
+                    this.logger.warn("Date tracker model has not been setup. Creating default profiles...");
+                }
+            });
         });
     }
 
