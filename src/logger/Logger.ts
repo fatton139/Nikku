15aebf3fbef848fb2ba2b68d7d86fa8cec97eef7
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-11-21 21:19:16 +1000

    Rework logger format for different transports

--- src/logger/Logger.ts
+++ src/logger/Logger.ts
@@ -1,27 +1,34 @@
+import * as moment from "moment";
 import * as winston from "winston";
-
-const { combine, timestamp, label, printf } = winston.format;
+import { ChannelTransport } from "./ChannelTransport";
 
 export class Logger {
     private readonly logger: winston.Logger;
     public constructor(className: string) {
         this.logger = winston.createLogger({
             format: winston.format.combine(
-                winston.format.colorize(),
-                label({ label: className }),
-                timestamp(),
-                printf((info) => {
-                    return `${info.timestamp}:${info.label}:${info.level}:${info.message}`;
-                }),
+                winston.format.label({ label: className }),
             ),
             transports: [
                 new winston.transports.File({
                     filename: "debug.log",
                     level: "info",
+                    format: winston.format.combine(
+                        winston.format.printf((info) => {
+                            return `${moment().format()}:${info.label}:${info.level}:${info.message}`;
+                        }),
+                    ),
                 }),
                 new winston.transports.Console({
                     level: "debug",
+                    format: winston.format.combine(
+                        winston.format.colorize(),
+                        winston.format.printf((info) => {
+                            return `${moment().format()}:${info.label}:${info.level}:${info.message}`;
+                        }),
+                    ),
                 }),
+                new ChannelTransport({}),
             ],
         });
     }
