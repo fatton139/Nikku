56a4d1ea08cf3b9d256b329422642d9b653c4361
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-11-21 21:18:47 +1000

    Add migration for dates and time

---
+++ src/database/migration/DateMigrator.ts
@@ -0,0 +1,24 @@
+import * as winston from "winston";
+import { Logger } from "logger/Logger";
+import { AccessLevel } from "user/AccessLevel";
+import { DBDateTracker } from "database/schemas/DBDateTracker";
+
+export class DateMigrator {
+    private readonly logger: winston.Logger = new Logger(this.constructor.name).getLogger();
+    private schema: DBDateTracker;
+    public constructor(schema: DBDateTracker) {
+        this.schema = schema;
+    }
+    public createModels(): void {
+        const dateModel = this.schema.getModelForClass(DBDateTracker);
+        const model = new dateModel({
+            startTime: new Date(),
+            shopLastUpdate: new Date(),
+        });
+        model.save().then((obj) => {
+            this.logger.info("DB Dates/Time setup.");
+        }).catch((err) => {
+            this.logger.error("Failed DB Dates/Time setup.");
+        });
+    }
+}
--- src/database/migration/UserMigrator.ts
+++ src/database/migration/UserMigrator.ts
@@ -1,6 +1,4 @@
-import * as Mongoose from "mongoose";
 import * as winston from "winston";
-import { Migrator } from "./Migrator";
 import { Logger } from "logger/Logger";
 import { DBUserSchema } from "database/schemas/DBUserSchema";
 import { AccessLevel } from "user/AccessLevel";
@@ -21,9 +19,9 @@ export class UserMigrator {
             });
             model.save().then((user) => {
                 i++;
-                this.logger.info("Dev user saved " + i + " of " + ids.length);
+                this.logger.info("Dev user saved " + i + " of " + ids.length + ".");
             }).catch((err) => {
-                this.logger.error("Failed to save user " + userId + ":" + err);
+                this.logger.error("Failed to save user " + userId + ":" + err + ".");
             });
         }
     }
