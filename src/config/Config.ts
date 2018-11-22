969cb209d14a17e6c4b771a39c5a582bfd5cbd4c
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-11-23 04:07:16 +1000

    Strip spaces and add more env vars in configs

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -7,13 +7,13 @@ namespace Config {
     const pjson = JSON.parse(fs.readFileSync("package.json", "utf8"));
     export class Discord {
         public static readonly TOKEN: string = process.env.DISCORD_TOKEN;
-        public static readonly DEBUG_CHANNELS: string[] = process.env.DEBUG_CHANNELS.split(",");
+        public static readonly DEBUG_CHANNELS: string[] = process.env.DEBUG_CHANNELS.replace(/\s/g, "").split(",");
     }
     export class Database {
         public static readonly URL: string = process.env.DATABASE_URL;
     }
     export class Command {
-        public static readonly PREFIXES: string[] = process.env.PREFIXES.split(",");
+        public static readonly PREFIXES: string[] = process.env.PREFIXES.replace(/\s/g, "").split(",");
         public static readonly COMMAND_SRC: string = "command/modules";
         public static readonly COMMAND_FULL_PATH: string = `${path.dirname(require.main.filename)}/src/${Command.COMMAND_SRC}`;
         public static readonly COMMAND_PATHS: string[] = [
@@ -23,6 +23,7 @@ namespace Config {
     export class Service {
         public static readonly CHATBOT_USER_ID: string = process.env.CHATBOT_USER_ID;
         public static readonly CHATBOT_API_KEY: string = process.env.CHATBOT_API_KEY;
+        public static readonly CHATBOT_SESSION: string = process.env.CHATBOT_SESSION;
     }
     export class Info {
         public static readonly GITHUB = pjson.repository;
@@ -30,7 +31,7 @@ namespace Config {
         public static readonly VERSION = pjson.version;
     }
     export class DefaultUser {
-        public static readonly IDS: string[] = process.env.DEV_IDS.split(",");
+        public static readonly IDS: string[] = process.env.DEV_IDS.replace(/\s/g, "").split(",");
     }
 }
 
