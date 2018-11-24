1dd6af32219ae8e9ba6db95647eb5a46ec1a2027
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-11-24 17:17:40 +1000

    Move core to member in event core and added base join/leave methods

--- src/core/EventCore.ts
+++ src/core/EventCore.ts
@@ -1,32 +1,46 @@
 import * as Discord from "discord.js";
+import * as winston from "winston";
+import Logger from "log/Logger";
 import NikkuCore from "core/NikkuCore";
 import OnMessageState from "state/OnMessageState";
 
 export default class EventCore {
-
+    private readonly logger: winston.Logger = new Logger(this.constructor.name).getLogger();
     /**
      * Main interface with Discord.js
      */
     private client: Discord.Client;
 
+    private core: NikkuCore;
+
     /**
      * @classdesc Class for handling events.
      * @param core - The main bot core.
      */
-    public constructor(client: Discord.Client) {
+    public constructor(client: Discord.Client, core: NikkuCore) {
         this.client = client;
     }
 
     /**
      * Begin listening for channel messages.
      */
-    public listenMessages(core: NikkuCore): void {
+    public listenMessages(): void {
         this.client.on("message", (message: Discord.Message) => {
             if (!message.author.bot) {
-                core.setCoreState(new OnMessageState(core, message));
-                core.getCommandManager().parseLine(message.content,
+                this.core.setCoreState(new OnMessageState(this.core, message));
+                this.core.getCommandManager().parseLine(message.content,
                     message.author.id);
             }
+        });
+    }
+
+    public handleGuildRegistration(): void {
+        this.client.on("guildCreate", (guild) => {
+            this.logger.verbose(`Joined new server "${guild.name}".`);
+        });
+
+        this.client.on("guildDelete", (guild) => {
+            this.logger.verbose(`Left server "${guild.name}".`);
         });
     }
 }
