5e1cb3e0e8d557cd359e83af8e3f3903b913c514
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-11-24 20:33:17 +1000

    Update db core and refactor model methods

--- src/core/DatabaseCore.ts
+++ src/core/DatabaseCore.ts
@@ -1,14 +1,20 @@
 import * as MongoDb from "mongodb";
 import * as Mongoose from "mongoose";
 import * as winston from "winston";
+import * as Discord from "discord.js";
 import DBUserSchema from "database/schemas/DBUserSchema";
-import DBDateTracker from "database/schemas/DBDateTracker";
+import DBGlobalPropertySchema from "database/schemas/DBGlobalPropertySchema";
+import DBGuildPropertySchema from "database/schemas/DBGuildPropertySchema";
 import Logger from "log/Logger";
 import UserMigrator from "database/migration/UserMigrator";
-import DateMigrator from "database/migration/DateMigrator";
+import GlobalPropertyMigrator from "database/migration/GlobalPropertyMigrator";
+import GuildPropertyMigrator from "database/migration/GuildPropertyMigrator";
+import NikkuCore from "./NikkuCore";
+import { getPriority } from "os";
 
 export default class DatabaseCore {
     private readonly logger: winston.Logger = new Logger(this.constructor.name).getLogger();
+    private client: Discord.Client;
     private readonly URL: string;
     /**
      * Database connection.
@@ -16,15 +22,23 @@ export default class DatabaseCore {
     private connection: Mongoose.Connection;
 
     private UserModel: Mongoose.Model<Mongoose.Document, {}>;
-    private DateTrackerModel: Mongoose.Model<Mongoose.Document, {}>;
+    private globalPropertyModel: Mongoose.Model<Mongoose.Document, {}>;
+    private guildPropertyModel: Mongoose.Model<Mongoose.Document, {}>;
 
     private defaultUsers: string[];
+
+    private core: NikkuCore;
+
+    private ready: boolean;
     /**
      * @classdesc Class for handling important database methods.
      */
-    public constructor(url: string, defaultUsers: string[]) {
-        this.URL = url;
-        this.defaultUsers = defaultUsers;
+    public constructor(core: NikkuCore) {
+        this.URL = core.getConfig().Database.URL;
+        this.defaultUsers = core.getConfig().DefaultUser.IDS;
+        this.client = core.getClient();
+        this.core = core;
+        this.ready = false;
     }
 
     /**
@@ -38,30 +52,59 @@ export default class DatabaseCore {
             throw new Error("Connection Error");
         });
         this.connection.once("open", () => {
-            this.logger.info("Database connected successfully.");
-            this.generateModelsIfEmpty();
+            this.generateModelsIfEmpty().then(() => {
+                this.ready = true;
+                this.logger.info("Database connected successfully.");
+            });
         });
     }
 
-    private generateModelsIfEmpty(): void {
+    public async generateDevUserModel(): Promise<void> {
         const userSchema: DBUserSchema = new DBUserSchema();
         this.UserModel = userSchema.getModelForClass(DBUserSchema);
-        this.UserModel.find({}).then((doc) => {
-            if (doc.length === 0) {
-                this.logger.warn("Dev user models has not been setup. Creating default profiles.");
-                const migrator = new UserMigrator(userSchema);
-                migrator.createModels(this.defaultUsers);
+        const doc: Mongoose.Document[] = await this.UserModel.find({});
+        if (doc.length === 0) {
+            this.logger.warn(`Dev user models not detected. Creating ${this.defaultUsers.length} dev profiles.`);
+            const migrator = new UserMigrator(userSchema);
+            await migrator.createModels(this.defaultUsers);
+        }
+        Promise.resolve();
+    }
+
+    public async generateGlobalPropertyModel(): Promise<void> {
+        const globalPropertySchema = new DBGlobalPropertySchema();
+        this.globalPropertyModel = globalPropertySchema.getModelForClass(DBGlobalPropertySchema);
+        const doc: Mongoose.Document[] = await this.globalPropertyModel.find({});
+        if (doc.length === 0) {
+            this.logger.warn("Global properties document has not been setup. Creating default profiles.");
+            const migrator = new GlobalPropertyMigrator(globalPropertySchema);
+            await migrator.createModels();
+        }
+        Promise.resolve();
+    }
+
+    public async generateGuildPropertyModel(): Promise<void> {
+        const guildPropertySchema = new DBGuildPropertySchema();
+        this.guildPropertyModel = guildPropertySchema.getModelForClass(DBGuildPropertySchema);
+        const docs = await this.guildPropertyModel.find({}) as any as DBGuildPropertySchema[];
+        for (const guild of this.client.guilds) {
+            if (docs.findIndex((sGuild) => sGuild.id === guild[1].id) === -1) {
+                const model = new this.guildPropertyModel({
+                    id: guild[1].id,
+                });
+                this.logger.warn(`Unregistered guild detected. Creating server property document for ${guild[1].name}.""`);
+                await model.save();
             }
-        });
-        const dateTrackerSchema = new DBDateTracker();
-        this.DateTrackerModel = dateTrackerSchema.getModelForClass(DBDateTracker);
-        this.DateTrackerModel.find({}).then((doc) => {
-            if (doc.length === 0) {
-                this.logger.warn("Dates and times model has not been setup. Creating default profiles.");
-                const migrator = new DateMigrator(dateTrackerSchema);
-                migrator.createModels();
-            }
-        });
+        }
+        Promise.resolve();
+    }
+
+    public async generateModelsIfEmpty(): Promise<void> {
+        await Promise.all([
+            this.generateDevUserModel(),
+            this.generateGlobalPropertyModel(),
+            this.generateGuildPropertyModel(),
+        ]);
     }
 
     /**
@@ -77,13 +120,22 @@ export default class DatabaseCore {
     public closeConnection(): void {
         this.logger.warn("Connection to DB closed.");
         this.connection.close();
+        this.ready = false;
     }
 
     public getUserModel(): Mongoose.Model<Mongoose.Document, {}> {
         return this.UserModel;
     }
 
-    public getDateTrackerModel(): Mongoose.Model<Mongoose.Document, {}> {
-        return this.DateTrackerModel;
+    public getGlobalPropertyModel(): Mongoose.Model<Mongoose.Document, {}> {
+        return this.globalPropertyModel;
+    }
+
+    public getGuildPropertyModel(): Mongoose.Model<Mongoose.Document, {}> {
+        return this.guildPropertyModel;
+    }
+
+    public isReady(): boolean {
+        return this.ready;
     }
 }
