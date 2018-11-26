df091a071e309bc5a256bae7b0a8324ddcf52525
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-11-26 21:59:21 +1000

    Added methods to register and default values for users

--- src/command/modules/user/Register.ts
+++ src/command/modules/user/Register.ts
@@ -1,1 +1,34 @@
-// Blank for testing
+import ExecutableCommand from "command/ExecutableCommand";
+import AccessLevel from "user/AccessLevel";
+import Action from "action/Action";
+import OnMessageState from "state/OnMessageState";
+import DBUserSchema from "database/schemas/DBUserSchema";
+
+export default class Register extends ExecutableCommand {
+    public constructor() {
+        super("register", AccessLevel.UNREGISTERED, 0, "Register yourself.");
+    }
+    public setCustomAction(): Action {
+        return new Action(async (state: OnMessageState) => {
+            const user = state.getMessageHandle().author;
+            const doc = await state.getDbCore().getUserModel().findOne({id: user.id});
+            if (!doc) {
+                const model = new DBUserSchema().getModelForClass(DBUserSchema);
+                try {
+                    await model.createNewUser(user.id);
+                    state.getMessageHandle().reply(
+                        "Successfully registered!" +
+                        " You now have access to **Level 1** Commands",
+                    );
+                    return true;
+                } catch (err) {
+                    state.getMessageHandle().reply("Failed to register.");
+                    return false;
+                }
+            } else {
+                state.getMessageHandle().reply("You are already registered.");
+                return true;
+            }
+        });
+    }
+}
--- src/database/schemas/DBUserSchema.ts
+++ src/database/schemas/DBUserSchema.ts
@@ -1,21 +1,33 @@
-import { prop, Typegoose, ModelType, InstanceType, instanceMethod } from "typegoose";
+import * as Mongoose from "mongoose";
+import * as winston from "winston";
+import Logger from "log/Logger";
+import { prop, Typegoose, ModelType, InstanceType, instanceMethod, staticMethod } from "typegoose";
 import AccessLevel from "user/AccessLevel";
 
 export default class DBUserSchema extends Typegoose {
+    private static readonly logger: winston.Logger = new Logger(DBUserSchema.constructor.name).getLogger();
     @prop({required: true, unique: true})
     private id: string;
 
     @prop({default: AccessLevel.UNREGISTERED})
     private accessLevel?: AccessLevel;
 
-    @prop()
-    private currency?: {};
+    @prop({default: {dotmaCoin: 100, bradCoin: 0}})
+    private currency?: {
+        dotmaCoin: number,
+        bradCoin: number,
+    };
 
-    @prop()
-    private daily?: {};
+    @prop({default: new Date()})
+    private daily?: {
+        lastUpdate: Date,
+    };
 
-    @prop()
-    private title?: {};
+    @prop({default: {active: 0, all: ["The Untitled"]}})
+    private title?: {
+        active: number,
+        all: string[];
+    };
 
     @instanceMethod
     public getId(): string {
@@ -23,12 +35,29 @@ export default class DBUserSchema extends Typegoose {
     }
 
     @instanceMethod
-    public setAccessLevel(level: AccessLevel) {
+    public setAccessLevel(level: AccessLevel): void {
         this.accessLevel = this.accessLevel;
     }
 
     @instanceMethod
     public getAccessLevel(): AccessLevel {
         return this.accessLevel ? this.accessLevel : AccessLevel.UNREGISTERED;
+    }
+
+    @staticMethod
+    public static async createNewUser(this: ModelType<DBUserSchema> & typeof DBUserSchema, id: string): Promise<any> {
+        const userModel = new DBUserSchema().getModelForClass(DBUserSchema);
+        const model = new userModel({
+            id,
+            accessLevel: AccessLevel.REGISTERED,
+        });
+        try {
+            const docs = await model.save();
+            DBUserSchema.logger.info("New user registered.");
+            Promise.resolve(docs);
+        } catch (err) {
+            DBUserSchema.logger.info(`Failed to register user:${err}`);
+            Promise.reject(err);
+        }
     }
 }
