9006b42c1106296b17db692ccb1f32e52b611a16
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-11-27 13:16:51 +1000

    Added method to test db ping and rename ping class

--- src/command/modules/util/Ping.ts
+++ src/command/modules/util/Ping.ts
@@ -3,7 +3,7 @@ import ExecutableCommand from "command/ExecutableCommand";
 import Action from "action/Action";
 import OnMessageState from "state/OnMessageState";
 
-export default class Test extends ExecutableCommand {
+export default class Ping extends ExecutableCommand {
     public constructor() {
         super("ping", AccessLevel.UNREGISTERED, 0, "Test command, responds with pong.");
     }
