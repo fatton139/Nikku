87d04e884eed5afd2423838df0418063540916b7
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-11-28 21:34:09 +1000

    Add message for unregistered user

--- src/command/modules/user/Profile.ts
+++ src/command/modules/user/Profile.ts
@@ -6,13 +6,22 @@ import DBUserSchema from "database/schemas/DBUserSchema";
 
 export default class Profile extends ExecutableCommand {
     public constructor() {
-        super("profile", AccessLevel.REGISTERED, 0, "View your profile.");
+        super("profile", AccessLevel.UNREGISTERED, 0, "View your profile.");
     }
     public setCustomAction(): Action {
         return new Action(async (state: OnMessageState) => {
             const user = state.getMessageHandle().author;
             try {
                 const doc = await state.getDbCore().getUserModel().findOne({id: user.id});
+                if (!doc) {
+                    state.getMessageHandle().reply(
+                        `\`\`\`` +
+                        `${user.username} - The unregistered.\n` +
+                        `You are not registered. type "!f register" to register.` +
+                        `\`\`\``,
+                    );
+                    return true;
+                }
                 const dbUser = doc as any as DBUserSchema;
                 const dateDiff = (new Date() as any) - (dbUser.dateRegistered as any);
                 const hours = dateDiff / (1000 * 60 * 60);
