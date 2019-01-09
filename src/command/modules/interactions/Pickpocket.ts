94c0359c192a37223d7e3c26e27216718b41fcc7
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-01-09 13:20:30 +1000

    Place command action under try catch block

--- src/command/modules/interactions/Pickpocket.ts
+++ src/command/modules/interactions/Pickpocket.ts
@@ -15,41 +15,47 @@ export default class Pickpocket extends ExecutableCommand {
 
     public setCustomAction(): Action {
         return new Action(async (state: OnMessageState) => {
-            const userModel = new DBUserSchema().getModelForClass(DBUserSchema);
-            const userId = state.getMessageHandle().author.id;
-            if (state.getMessageHandle().mentions.everyone)  {
-                state.getMessageHandle().channel.send("You cannot thieve everyone at once.");
+            try {
+                const userModel = new DBUserSchema().getModelForClass(DBUserSchema);
+                const userId = state.getMessageHandle().author.id;
+                if (state.getMessageHandle().mentions.everyone)  {
+                    state.getMessageHandle().channel.send("You cannot thieve everyone at once.");
+                    return false;
+                }
+                const targetId = state.getMessageHandle().mentions.users.first().id;
+                if (userId === targetId) {
+                    state.getMessageHandle().channel.send("You cannot thieve yourself.");
+                    return false;
+                }
+                const dbUser = await userModel.getUser(userId);
+                const dbTarget = await userModel.getUser(targetId);
+                if (!dbTarget) {
+                    state.getMessageHandle().channel.send("Target is not registered.");
+                    return false;
+                }
+                const levelDifference = (await dbTarget.getSkillLevel(SkillType.THIEVING)) -
+                    (await dbUser.getSkillLevel(SkillType.THIEVING));
+                let targetChance = 55 - levelDifference;
+                targetChance = targetChance < 5 ? 5 : targetChance;
+                targetChance = targetChance > 95 ? 95 : targetChance;
+                if (randInt(0, 100) < targetChance) {
+                    let coinsStolen = 15 + (await dbUser.getSkillLevel(SkillType.THIEVING)) + levelDifference;
+                    coinsStolen = coinsStolen < 5 ? 5 : coinsStolen;
+                    let experienceGained = (await dbUser.getSkillLevel(SkillType.THIEVING) * 10) + levelDifference;
+                    experienceGained = experienceGained < 30 ? 30 : experienceGained;
+                    state.getMessageHandle().channel.send(`Successfully thieved ${coinsStolen} coins from <@!${targetId}>. `
+                    + `Gained ${experienceGained} experience.`);
+                    await dbUser.addSkillExperience(SkillType.THIEVING, experienceGained);
+                    await dbUser.addCurrency(CoinType.DOTMA_COIN, coinsStolen);
+                    await dbTarget.removeCurrency(CoinType.DOTMA_COIN, coinsStolen);
+                } else {
+                    state.getMessageHandle().channel.send("You failed to pickpocket.");
+                }
+                return true;
+            } catch (err) {
+                this.logger.warn(err);
                 return false;
             }
-            const targetId = state.getMessageHandle().mentions.users.first().id;
-            if (userId === targetId) {
-                state.getMessageHandle().channel.send("You cannot thieve yourself.");
-                return false;
-            }
-            const dbUser = await userModel.getUser(userId);
-            const dbTarget = await userModel.getUser(targetId);
-            if (!dbTarget) {
-                state.getMessageHandle().channel.send("Target is not registered.");
-                return false;
-            }
-            const levelDifference = (await dbTarget.getSkillLevel(SkillType.THIEVING)) - (await dbUser.getSkillLevel(SkillType.THIEVING));
-            let targetChance = 55 - levelDifference;
-            targetChance = targetChance < 5 ? 5 : targetChance;
-            targetChance = targetChance > 95 ? 95 : targetChance;
-            if (randInt(0, 100) < targetChance) {
-                let coinsStolen = 15 + (await dbUser.getSkillLevel(SkillType.THIEVING)) + levelDifference;
-                coinsStolen = coinsStolen < 5 ? 5 : coinsStolen;
-                let experienceGained = (await dbUser.getSkillLevel(SkillType.THIEVING) * 10) + levelDifference;
-                experienceGained = experienceGained < 30 ? 30 : experienceGained;
-                state.getMessageHandle().channel.send(`Successfully thieved ${coinsStolen} coins from <@!${targetId}>. `
-                + `Gained ${experienceGained} experience.`);
-                await dbUser.addSkillExperience(SkillType.THIEVING, experienceGained);
-                await dbUser.addCurrency(CoinType.DOTMA_COIN, coinsStolen);
-                await dbTarget.removeCurrency(CoinType.DOTMA_COIN, coinsStolen);
-            } else {
-                state.getMessageHandle().channel.send("You failed to pickpocket.");
-            }
-            return true;
         });
     }
 }
