898137684c0124254ca9d3bfa9501216bd9b20d8
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-01-19 19:34:38 +1000

    Added CommandUtil

--- src/utils/CommandUtil.ts
+++ src/utils/CommandUtil.ts
@@ -1,9 +1,25 @@
+import Config from "config/Config";
+import { LevenshteinDistance } from "math/LevenshteinDistance";
 
-/**
- * Extracts the user id from a string.
- * @param text - The text to extract id from.
- */
-export const getId = (text: string): string => {
-    return text.charAt(2) !== "!" ?
-        text.slice(2, text.length - 1) : text.slice(3, text.length - 1);
-};
+export namespace CommandUtil {
+    /**
+     * Extracts the user id from a string.
+     * @param text - The text to extract id from.
+     */
+    export const getId = (text: string): string => {
+        return text.charAt(2) !== "!" ?
+            text.slice(2, text.length - 1) : text.slice(3, text.length - 1);
+    };
+
+    export const isResponseTrigger = (message: string, tol: number): boolean => {
+        const messageArr = message.split(" ");
+        const triggerWord = Config.Command.BOT_RESPONSE_TRIGGER.replace(/\s/g, "").toLowerCase();
+        for (let i = 1; i < messageArr.length; i++) {
+            if (LevenshteinDistance((messageArr[i - 1] + messageArr[i]).toLowerCase(), triggerWord) <= tol) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+}
