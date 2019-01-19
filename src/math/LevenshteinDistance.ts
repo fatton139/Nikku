5f93a9c8b26759110d6673e6a30e216f179f37bc
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-01-19 18:36:04 +1000

    Added levenshtein distance calculator

---
+++ src/math/LevenshteinDistance.ts
@@ -0,0 +1,26 @@
+import { MathUtil } from "math/MathUtil";
+
+export const LevenshteinDistance = (a: string, b: string) => {
+    if (a.length === 0 || b.length === 0) {
+        return a.length === 0 ? b.length : a.length;
+    }
+    const matrix: number[][] = MathUtil.zeros(a.length + 1, b.length + 1);
+    for (let i = 1; i < matrix.length; i++) {
+        matrix[i][0] = i;
+    }
+    for (let i = 1; i < matrix[0].length; i++) {
+        matrix[0][i] = i;
+    }
+    for (let i = 1; i <= b.length; i++) {
+        for (let j = 1; j <= a.length; j++) {
+            b[i - 1] === a[j - 1] ?
+            matrix[i][j] = matrix[i - 1][j - 1] :
+            matrix[i][j] = Math.min(
+                Number(matrix[i - 1][j]) + 1,
+                Number(matrix[i][j - 1]) + 1,
+                Number(matrix[i - 1][j - 1]) + 1,
+            );
+        }
+    }
+    return matrix[b.length][a.length];
+};
