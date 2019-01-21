52dd50aa188cc77db348df662ad31607c7bddf75
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-01-21 14:56:18 +1000

    Using namespace for config

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -5,45 +5,45 @@ import * as path from "path";
 export namespace Config {
     dotenvConfig();
     const pjson = JSON.parse(fs.readFileSync("package.json", "utf8"));
-    export class Discord {
-        public static readonly TOKEN: string = process.env.DISCORD_TOKEN;
-        public static readonly DEBUG_CHANNELS: string[] = process.env.DEBUG_CHANNELS ?
+    export namespace Discord {
+        export const TOKEN: string = process.env.DISCORD_TOKEN;
+        export const DEBUG_CHANNELS: string[] = process.env.DEBUG_CHANNELS ?
             process.env.DEBUG_CHANNELS.replace(/\s/g, "").split(",") : undefined;
     }
-    export class Database {
-        public static readonly URL: string = process.env.DATABASE_URL;
+    export namespace Database {
+        export const URL: string = process.env.DATABASE_URL;
     }
-    export class Command {
-        public static readonly BOT_RESPONSE_TRIGGER = "Mr Fortnite";
-        public static readonly PREFIXES: string[] = process.env.PREFIXES ?
+    export namespace Command {
+        export const BOT_RESPONSE_TRIGGER = "Mr Fortnite";
+        export const PREFIXES: string[] = process.env.PREFIXES ?
             process.env.PREFIXES.replace(/\s/g, "").split(",") : ["!f"];
-        public static readonly COMMAND_SRC: string = "command/modules";
-        public static readonly COMMAND_FULL_PATH: string =
-            `${path.dirname(require.main.filename)}/src/${Command.COMMAND_SRC}`;
-        public static readonly COMMAND_PATHS: string[] =
+        export const COMMAND_SRC = "command/modules";
+        export const COMMAND_FULL_PATH = `${path.dirname(require.main.filename)}/src/${Command.COMMAND_SRC}`;
+        export const COMMAND_PATHS: string[] =
         [
             "mrfortnite",
             "util",
             "user",
             "brad",
             "interactions",
+            "config",
         ];
     }
-    export class Service {
-        public static readonly CHATBOT_USER_ID: string = process.env.CHATBOT_USER_ID;
-        public static readonly CHATBOT_API_KEY: string = process.env.CHATBOT_API_KEY;
-        public static readonly CHATBOT_SESSION: string = process.env.CHATBOT_SESSION;
+    export namespace Service {
+        export const CHATBOT_USER_ID: string = process.env.CHATBOT_USER_ID;
+        export const CHATBOT_API_KEY: string = process.env.CHATBOT_API_KEY;
+        export const CHATBOT_SESSION: string = process.env.CHATBOT_SESSION;
     }
-    export class Info {
-        public static readonly GITHUB = pjson.repository;
-        public static readonly AUTHOR = pjson.author;
-        public static readonly VERSION = pjson.version;
+    export namespace Info {
+        export const GITHUB = pjson.repository;
+        export const AUTHOR = pjson.author;
+        export const VERSION = pjson.version;
     }
-    export class DefaultUser {
-        public static readonly IDS: string[] = process.env.DEV_IDS ?
+    export namespace DefaultUser {
+        export const IDS: string[] = process.env.DEV_IDS ?
             process.env.DEV_IDS.replace(/\s/g, "").split(",") : undefined;
     }
-    export class Brad {
-        public static readonly DEFAULT_WEIGHT: number = 200;
+    export namespace Brad {
+        export const DEFAULT_WEIGHT = 200;
     }
 }
