cfedfaaa8876b6f399ab89caf87dcd5e02e3f00e
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-01-21 15:59:17 +1000

    Added command to view current configs

---
+++ src/command/modules/config/ViewConfig.ts
@@ -0,0 +1,38 @@
+import * as Discord from "discord.js";
+import ExecutableCommand from "command/ExecutableCommand";
+import { AccessLevel } from "user/AccessLevel";
+import Action from "action/Action";
+import OnMessageState from "state/OnMessageState";
+import DBGuildPropertySchema from "database/schemas/DBGuildPropertySchema";
+import { Config } from "config/Config";
+
+export default class ViewConfig extends ExecutableCommand {
+    public constructor() {
+        super("viewconfig", AccessLevel.MODERATOR, 0, "View bot configurations.");
+    }
+    public setCustomAction(): Action {
+        return new Action(async (state: OnMessageState): Promise<boolean> => {
+            try {
+                const model = DBGuildPropertySchema.getModel();
+                const guild = await model.getGuildById(state.getMessageHandle().guild.id);
+                const configs = await guild.getAllBooleanConfig();
+                const embed = new Discord.MessageEmbed();
+                embed.setTitle("Bot Configurations.");
+                embed.setDescription("Current Bot Configurations. Edit with the **setConfig** command.");
+                embed.setColor(0xE600AC);
+                embed.setAuthor(state.getCore().getClient().user.username);
+                let values = "";
+                for (const key of Object.keys(configs)) {
+                    values += `${key} - ${configs[key]}\n`;
+                }
+                embed.addField("Boolean Configurations", values);
+                embed.setTimestamp(new Date());
+                embed.setFooter(Config.Command.BOT_RESPONSE_TRIGGER);
+                state.getMessageHandle().channel.send(embed);
+                return true;
+            } catch (err) {
+                throw err;
+            }
+        });
+    }
+}
