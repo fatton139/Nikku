9b0f36979e31b60b885d6771c9ade077f3a6b362
Thomas Hinchliff <thinc360@gmail.com>
2019-02-06 16:25:10 +1000

    Create Kiss.ts
    kiss Owo

---
+++ src/command/modules/interactions/Kiss.ts
@@ -0,0 +1,46 @@
+import { AccessLevel } from "user/AccessLevel";
+import ExecutableCommand from "command/ExecutableCommand";
+import Action from "action/Action";
+import OnMessageState from "state/OnMessageState";
+import DBUserSchema from "database/schemas/DBUserSchema";
+import { CoinType } from "user/CoinType";
+
+/**
+ * Allows a user to kiss another user :)
+ * 
+ * returns boolean indicating success of command.
+ */
+export default class Pickpocket extends ExecutableCommand {
+    public constructor() {
+        super("pickpocket", AccessLevel.REGISTERED, 1, "Attempt to steal coins from someone.", "!f thieve [target]");
+    }
+
+    public setCustomAction(): Action {
+        return new Action(async (state: OnMessageState) => {
+            try {
+                const userModel = DBUserSchema.getModel();
+                const userId = state.getMessageHandle().author.id;
+                if (state.getMessageHandle().mentions.everyone)  {
+                    state.getMessageHandle().channel.send("You cannot kiss everyone at once OwO.");
+                    return false;
+                }
+                const targetId = state.getMessageHandle().mentions.users.first().id;
+                if (userId === targetId) {
+                    state.getMessageHandle().channel.send("You cannot kiss yourself silly xD.");
+                    return false;
+                }
+                const dbUser = await userModel.getUserById(userId);
+                const dbTarget = await userModel.getUserById(targetId);
+                if (!dbTarget) {
+                    state.getMessageHandle().channel.send("Kissing Target is not registered.");
+                    return false;
+                }
+                state.getMessageHandle().channel.send(`<@!${userId}> kissed <@!${targetId}> OwO kawaii!`);
+                return true;
+            } catch (err) {
+                this.logger.warn(err);
+                return false;
+            }
+        });
+    }
+}
