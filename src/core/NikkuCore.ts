ec68c45548a7aa2791ee5f37e2a97839a370585c
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-09-14 03:20:30 +1000

    Added config parsing methods on core

--- src/core/NikkuCore.ts
+++ src/core/NikkuCore.ts
@@ -34,35 +34,61 @@ export class NikkuCore {
 
     private managers: Map<string, AbstractManager>;
 
-    private botConfigOptions: BotConfigOptions;
+    private botConfigOptions?: BotConfigOptions;
 
-    private pjsonData: PackagejsonData;
+    private pjsonData?: PackagejsonData;
+
     private static instance: NikkuCore;
 
     /**
      * @param config Initial configurations for the bot.
      * @param initializeImmediately Start main processes immediately.
      */
-    public constructor(initializeImmediately?: boolean) {
-        const configParser = new ConfigParser();
-        configParser.parseEnvConfig();
-        this.botConfigOptions = configParser.getBotConfig();
-        this.pjsonData = configParser.getPackageJSONData();
+    public constructor(coreInitializer: CoreInitializer) {
+        this.logger.debug("Nikku Core initialized.");
         this.client = new Discord.Client();
         this.managers = new Map<string, AbstractManager>();
         this.eventCore = new EventCore(this);
-        this.databaseCore = new DatabaseCore();
-        this.logger.debug("Core initialized.");
-        if (initializeImmediately) {
+        this.databaseCore = new DatabaseCore(this);
+        this.retrieveInitializationConfiguration(new ConfigParser(coreInitializer.configurationPath));
+        if (coreInitializer.initializeImmediately) {
             this.startMainProcesses();
         }
         NikkuCore.instance = this;
+    }
+
+    private retrieveInitializationConfiguration(configParser: ConfigParser): void {
+        try {
+            configParser.parseConfig().parsePackageJSON().parseEnvConfig();
+            this.botConfigOptions = configParser.getBotConfig();
+            this.pjsonData = configParser.getPackageJSONData();
+        }  catch (e) {
+            this.logger.error("Error while parsing configurations.");
+            process.exit();
+        }
+    }
+
+    private validateEnvironmentalVariables(): void {
+        let exception: NikkuException | undefined;
+        if (!NikkuConfig.EnvironmentVariables.DiscordOptions.TOKEN) {
+            exception = new NikkuException("Missing Discord Bot Token.");
+        }
+        // Additional checking for other options.
+        if (exception) {
+            throw exception;
+        }
     }
 
     /**
      * Start the main processes of the bot.
      */
     public startMainProcesses(): void {
+        try {
+            this.validateEnvironmentalVariables();
+        } catch (e) {
+            this.logger.error(e.message);
+            return;
+        }
         this.client.login(NikkuConfig.EnvironmentVariables.DiscordOptions.TOKEN);
         this.client.on(EventType.READY, async () => {
             this.setDebugLogChannels();
@@ -77,13 +103,14 @@ export class NikkuCore {
      * Starts database related processes.
      */
     public async startDbProcesses(): Promise<boolean> {
+        const version: string | undefined = this.pjsonData ? this.pjsonData.VERSION : "0.0.0";
         try {
             await this.databaseCore.connectDb();
-            this.logger.info(`Nikku v${this.pjsonData.VERSION} started.`);
+            this.logger.info(`Nikku v${version ? version : "0.0.0"} started.`);
             return true;
         } catch (err) {
             // no db mode.
-            this.logger.warn(`Nikku v${this.pjsonData.VERSION} started without an database.`);
+            this.logger.warn(`Nikku v${version} started without an database.`);
             this.logger.error(err.message);
             return false;
         }
@@ -141,11 +168,11 @@ export class NikkuCore {
     /**
      * @returns The loaded package.json data.
      */
-    public getPackageJsonData(): PackagejsonData {
+    public getPackageJsonData(): PackagejsonData | undefined {
         return this.pjsonData;
     }
 
-    public getBotConfigOptions(): BotConfigOptions {
+    public getBotConfigOptions(): BotConfigOptions | undefined {
         return this.botConfigOptions;
     }
 
